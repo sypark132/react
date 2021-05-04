896139556f743c4fca5aa3888b25c9d782584736
seoyoon park <yeggam12@gmail.com>
2021-05-04 12:32:18 +0900

    add useRef

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,21 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample() {
-const [text, setText] = useState('');
+const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+});
+const nameInput = useRef();
+const {name, nickname} = inputs;
+
 const onChange = (e) => {
-    setText(e.target.value);
+    const {name, value} = e.target;
+    {/* react에서 객체를 업데이트 할 때에는 객체를 일단 복사한다.
+     */}
+    const nextInputs = {
+        ...inputs,
+        [name]: value,
+    };
+    setInputs(nextInputs)
 }
 const onReset = () => {
-    setText('');
+    setInputs({
+        name: '',
+        nickname: '',
+    });
+    nameInput.current.focus();
 }
     return (
         <div>
             {/* value값이 없으면 초기화할 때 input 박스에 있는 값은 초기화되지 않음 */}
-            <input onChange={onChange} value={text}/>
+            <input 
+                name="name" 
+                placeholder="이름" 
+                onChange={onChange} 
+                value={name} 
+                ref={nameInput}
+            />
+            <input 
+                name="nickname" 
+                placeholder="닉네임" 
+                onChange={onChange} 
+                value={nickname} 
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값 : </b>
-                {text}
+                {name} ({nickname})
             </div>
         </div>
 
