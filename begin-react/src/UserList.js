dc8d893b083f7305c9ffd8874bfea693f57ac21e
seoyoon park <yeggam12@gmail.com>
2021-05-07 11:00:11 +0900

    useEffect

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({user, onRemove, onToggle}) {
     const {username, email, id, active} = user;
+    // useEffect(() => {
+    //     console.log('컴포넌트가 화면에 나타남');
+    //     // props -> state
+    //     // Rest API
+    //     // D3 Video.js
+    //     // setInterval, setTimeout
+    //     return () => {
+    //         // clearInterval, clearTimeout
+    //         // 라이브러리 인스턴스 제거
+    //         console.log('컴포넌트가 화면에서 사라짐')
+    //     }
+    // },[]);
+    useEffect(() => {
+        //컴포넌트가 렌더링되자마자 실행
+        console.log('user값이 설정됨')
+        console.log(user);
+        return () => {
+            //clean함수(뒷처리함수), [user]가 업데이트 되기 직전에 실행
+            console.log('user값이 바뀌기 전');
+            console.log(user);
+        }
+        //props를 가져왔다면 무조건 넣어주어야 한다.
+    }, [user]);
+
     return (
         <div>
             <b style={{
