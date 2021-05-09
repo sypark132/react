3dd134c8d6b2bf3fa2418fe3f533018725b88036
seoyoon park <yeggam12@gmail.com>
2021-05-09 12:50:22 +0900

    add id4

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -43,7 +43,13 @@ function App() {
         username: 'react',
         email: 'react@react.com',
         active: false
-    }
+    },
+    {
+      id: 4,
+      username: 'sysy',
+      email: 'sy55@test.com',
+      active: true
+  }
   ]);
 
 
@@ -84,7 +90,7 @@ function App() {
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   //useMemo는 users가 바뀔 때에만 호출이 되고 그렇지 않으면
-  
+
   //이전에 만들어놨던 값을 재사용한다.
 
   return (
